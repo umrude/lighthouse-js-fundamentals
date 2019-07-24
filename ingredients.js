b171719bedbb298c62dafa4c958b00eab463c254
Gabi Geollegue <gabigeollegue@gmail.com>
2019-07-24 00:55:41 +0000

    2 ways of iterating over arrays and 1 way of printing an array backwards

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+var i = 0
+while (i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (i = 0; i <ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+var newIngredients = ingredients.reverse();
+for (i = 0; i < newIngredients.length; i++) {
+  console.log(newIngredients[i]);
+}
